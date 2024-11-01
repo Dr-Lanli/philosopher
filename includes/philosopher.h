e6373abb7295f77e5aeaf4a524314baf80b5ce98
Lilian Monsat <lmonsat@c2r9s1.42luxembourg.lu>
2024-11-01 03:19:03 +0100

    update

--- includes/philosopher.h
+++ includes/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: lmonsat <lmonsat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 15:55:51 by lmonsat           #+#    #+#             */
-/*   Updated: 2024/10/31 18:42:37 by lmonsat          ###   ########.fr       */
+/*   Updated: 2024/11/01 03:16:28 by lmonsat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include <stdio.h>
 # include <pthread.h>
 # include <unistd.h>
+
+struct s_philosopher 
+{
+    int has_eaten;
+    int has_slept;
+    int has_thought;
+};
 
 
 #endif
